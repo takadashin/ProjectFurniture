e8aaf683fd9d4fac98a6197a6419d7e32da5266c
takadashin <khaivo17@yahoo.com>
2015-12-05 11:31:19 -0500

    commit

--- Furniture/src/java/com/furniture/bean/ShippingBean.java
+++ Furniture/src/java/com/furniture/bean/ShippingBean.java
@@ -12,6 +12,7 @@ import com.furniture.utils.Criterion;
 import com.sun.corba.se.impl.orbutil.closure.Constant;
 import java.util.List;
 import java.util.Vector;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -22,5 +23,52 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean (name="shippingBean", eager = true)
 @RequestScoped
 public class ShippingBean extends Shipping{    
-	
+	private ShippingService Service = new ShippingService();
+    private Shipping Shipvar;
+    private Vector<Shipping> Shiparray;
+
+    @PostConstruct
+    public void init() {
+        Shiparray = Service.getAll();
+    }
+
+    public ShippingService getService() {
+        return Service;
+    }
+
+    public void setService(ShippingService Service) {
+        this.Service = Service;
+    }
+
+    public Shipping getShipvar() {
+        return Shipvar;
+    }
+
+    public void setShipvar(Shipping Shipvar) {
+        this.Shipvar = Shipvar;
+    }
+
+    public Vector<Shipping> getShiparray() {
+        return Shiparray;
+    }
+
+    public void setShiparray(Vector<Shipping> Shiparray) {
+        this.Shiparray = Shiparray;
+    }
+
+    public String delete() {
+        
+        Service.deleteById(Shipvar.getId());
+        return  "shipping?faces-redirect=true";
+    }
+        public String update() {
+        ShippingService service = new ShippingService();
+        Vector<Criterion> argument = new Vector<>();
+        Criterion object = new Criterion(Constants.SHIPPING_NAME, Shipvar.getShippingName());
+        //Criterion object2 = new Criterion(Constants.SHIPPING_PRICE, this.getShippingPrice().toString());
+        argument.add(object);
+       // argument.add(object2);
+        service.updatedByID(argument,Shipvar.getId());
+        return  "shipping?faces-redirect=true";
+    }
 }
