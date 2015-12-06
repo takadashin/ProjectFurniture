6315d3baaf54f08cdd37730273a351ded963f7b2
linhngoc1841 <linhngoc1841@yahoo.com>
2015-12-05 19:12:06 -0500

    tax

---
+++ Furniture/src/java/com/furniture/dao/ProductDao.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.furniture.dao;
+
+import com.furniture.domain.Product;
+import com.furniture.utils.Constants;
+import com.furniture.utils.Criterion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Linh
+ */
+public class ProductDao  extends BaseDao<Product>{
+    public ProductDao(){
+        tableName = Constants.PRODUCT_TB;
+    }
+    @SuppressWarnings("unchecked")
+    public void convertToDomain(ResultSet data, Vector<Product> products)
+    {
+        try {
+            if(data != null){
+                while(data.next())
+                {
+                    Product product =new Product();
+                    product.setId(data.getInt(Constants.ID));
+                    product.setCatId(data.getInt(Constants.PRODUCT_CAT_ID));
+                    product.setCode(data.getString(Constants.PRODUCT_CODE));
+                    product.setCost(data.getFloat(Constants.PRODUCT_COST));
+                    product.setDesc(data.getString(Constants.PRODUCT_DESC));
+                    product.setName(data.getString(Constants.PRODUCT_NAME));
+                    product.setPrice(data.getFloat(Constants.PRODUCT_PRICE));
+                    product.setQuantity(data.getInt(Constants.PRODUCT_QTY));
+                    product.setShortDesc(data.getString(Constants.PRODUCT_SHORT_DESC));
+                    product.setSpecPrice(data.getFloat(Constants.PRODUCT_SPEC_PRICE));
+                    product.setPostDate(data.getDate(Constants.PRODUCT_POST_DATE));
+                    
+                    products.add(product);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    public Vector convertToTbData(ResultSet data, int colCount)
+    {
+        Vector products = new Vector<>();
+        try {
+            if(data != null){
+                while(data.next())
+                {
+                    Vector record = new Vector();
+                    for (int i = 0; i < colCount; i++) {
+                        record.add(data.getString(i + 1));
+                    }
+                    products.add(record);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return products;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public Object convertToData(Product product,Vector<Criterion> data)
+    {
+        Object id =product.getId();
+        data.add(new Criterion(Constants.PRODUCT_CAT_ID, product.getCatId()));
+        data.add(new Criterion(Constants.PRODUCT_CODE, product.getCode()));
+        data.add(new Criterion(Constants.PRODUCT_COST, product.getCost()));
+        data.add(new Criterion(Constants.PRODUCT_DESC, product.getDesc()));
+        data.add(new Criterion(Constants.PRODUCT_NAME, product.getName()));
+        data.add(new Criterion(Constants.PRODUCT_PRICE, product.getPrice()));
+        data.add(new Criterion(Constants.PRODUCT_QTY, product.getQuantity()));
+        data.add(new Criterion(Constants.PRODUCT_SHORT_DESC, product.getShortDesc()));
+        data.add(new Criterion(Constants.PRODUCT_SPEC_PRICE, product.getSpecPrice()));
+        data.add(new Criterion(Constants.PRODUCT_POST_DATE, product.getPostDate()));
+
+        return id;
+    }
+}
